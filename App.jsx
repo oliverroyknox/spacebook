22b1c52e59be7adb6b0ebdd007de89e97b07dac7
Oliver Knox <dev@oliverroyknox.com>
2022-02-01 14:49:55 +0000

    Set up navigation and added placeholder components for pages.

--- App.jsx
+++ App.jsx
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import ProfilePage from './pages/ProfilePage';
+import SearchPage from './pages/SearchPage';
+import FriendsPage from './pages/FriendsPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  /**
+   * Callback to set `isAuthenticated` state.
+   */
+  const onAuthenticate = () => {
+    setIsAuthenticated(true);
+  };
+
+  setTimeout(onAuthenticate, 3000); // TODO: Placeholder for authentication flow.
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      { isAuthenticated ? (
+        <Tab.Navigator>
+          <Stack.Screen name="Profile" component={ProfilePage} />
+          <Stack.Screen name="Search" component={SearchPage} />
+          <Stack.Screen name="Friends" component={FriendsPage} />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginPage} />
+          <Stack.Screen name="Signup" component={SignupPage} />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
   );
 }
